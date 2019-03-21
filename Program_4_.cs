f665655e57db954694ec33cbc1b14b250e6ea361
LDPNam18i1 <48077469+LDPNam18i1@users.noreply.github.com>
2019-03-21 22:47:30 +0700

    Add files via upload

---
+++ Program_4_.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien
+{
+    class Program_4_
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+            Console.WriteLine("Nhap so sinh vien la");
+            n = int.Parse(Console.ReadLine());
+            // tao mang doi tuong Sv
+            QuanLySV_4_[] sv = new QuanLySV_4_[n];
+            for (int i = 0; i < n; i++)
+            {
+                sv[i] = new QuanLySV_4_();
+                sv[i].nhapTT();
+
+
+
+
+            }
+            Console.WriteLine("Danh sach Sinh Vien");
+            for (int j = 0; j < n; j++)
+            {
+                sv[j].In();
+            }
+            Console.ReadKey();
+
+
+            QuanLySV_4_ tmp;
+            tmp = sv[0];
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = i + 1; j < n; j++)
+
+                        if (sv[i].diemtb() < sv[j].diemtb())
+                        {
+                            tmp = sv[i];
+                            sv[i] = sv[j];
+                            sv[j] = tmp;
+                        }
+                }
+            }
+
+            Console.WriteLine("sinh vien sap xep theo thu tu giam dan");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("SV co diem tb {0} ", sv[i].diemtb());
+            }
+            Console.ReadLine();
+
+
+            Console.WriteLine("Nhap ten  sinh vien can tim");
+            string p = Console.ReadLine();
+            for (int i = 0; i < n; i++) 
+                if (sv[i].tenSV.Contains(p))
+                    Console.WriteLine("Ten SV muon tim la: Ten: {0}, Ma SV: {1}, Diem TB: {2}  ", sv[i].tenSV, sv[i].maSV, sv[i].diemtb());
+
+           
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
+
