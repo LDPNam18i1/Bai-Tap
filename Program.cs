3690cc3a54d6359186d86cc297ef449b7c49c9cd
LDPNam18i1 <48077469+LDPNam18i1@users.noreply.github.com>
2019-03-21 21:38:04 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,28 +4,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BaiKiemTra
+
+namespace QuanLySinhVien
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // bai 1
-            TinhTong tong = new TinhTong();
-            tong.nhapn();
-            tong.tinhTong();
-            tong.inTong();
-            tong.tinhTong2();
-            tong.tinhTong3();
-            tong.tongUoc();
 
-            // bai 2
-            DoanTau tau = new DoanTau();
-            tau.nhapMaDoanTau();
-            tau.tinhTien();
+            int n;
+            Console.WriteLine("Nhap so sinh vien");
+            n = int.Parse(Console.ReadLine());
+            // tao mang doi tuong Sv
+            SinhVien[] sv = new SinhVien[n];
+            for (int i = 0; i < n; i++)
+            {
+                sv[i] = new SinhVien();
+                sv[i].nhapTT();
+                
 
-            tau.inMatau();
+
+
+            }
+            Console.WriteLine("Danh sach Sinh Vien");
+            for (int j = 0; j < n; j++)
+            {
+                sv[j].In();
+            }
+            Console.ReadKey();
+
+
+            SinhVien tmp;
+            tmp = sv[0];
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = i + 1; j < n; j++)
+
+                        if (sv[i].diemtb() < sv[j].diemtb())
+                        {
+                            tmp = sv[i];
+                            sv[i] = sv[j];
+                            sv[j] = tmp;
+                        }
+                }
+            }
+
+            Console.WriteLine("sinh vien sap xep theo thu tu giam dan");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("SV co diem tb {0} ", sv[i].diemtb());
+            }
             Console.ReadLine();
+
+
+            Console.WriteLine("Nhap ten  sinh vien can tim");
+            string p = Console.ReadLine();
+            for (int i = 0; i < n; i++)
+                if (sv[i].tenSV.Contains(p))
+                    Console.WriteLine("Ten SV muon tim la: Ten: {0}, Ma SV: {1}, Diem TB: {2}  ", sv[i].tenSV,sv[i].maSV,sv[i].diemtb());
+            
+
+            Console.ReadLine();
+
 
 
 
---
+++ SinhVien.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien
+{
+    class SinhVien
+    {
+        
+        public String maSV;
+        public String tenSV;
+        public float diemTB;
+
+        //default constructors
+        public SinhVien()
+        {
+            this.maSV = "01";
+            this.tenSV = "Le Dinh Phuong Nam";
+            this.diemTB = (float)8.0;
+
+        }
+        //tham so hoa constructors
+        public SinhVien(String maSV, String tenSV, float diemTB)
+        {
+            this.maSV = maSV;
+            this.tenSV = tenSV;
+            this.diemTB = diemTB;
+        }
+
+
+
+
+
+        public void nhapTT()
+
+        {
+
+
+
+            Console.Write("Nhap Ten SV: ");
+            this.tenSV = Console.ReadLine();
+
+            Console.Write("Nhap ma SV: ");
+            this.maSV = Console.ReadLine();
+
+            Console.Write("Nhap Diem SV:");
+            this.diemTB = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("--------------");
+
+
+
+
+
+        }
+
+        public void In()
+        {
+            Console.WriteLine("Ma SV: {0} ", this.maSV);
+            Console.WriteLine("Ten SV: {0} ", this.tenSV);
+            Console.WriteLine("Diem TB SV: {0} ", this.diemTB);
+            Console.WriteLine("\n--------------------------\n");
+
+        }
+
+        public float diemtb()
+        {
+            return this.diemTB;
+        }
+
+     
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
+    
+
